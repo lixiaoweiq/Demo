deaf14dedf6ab23bd7c62b12285cfe828dd8ca01
Lixiaowei <apple@MacBook-Pro.lan>
2016-11-23 14:44:13 +0800

    1

---
+++ Demo/QViewController.h
@@ -0,0 +1,13 @@
+//
+//  QViewController.h
+//  Demo
+//
+//  Created by apple on 16/11/23.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QViewController : UIViewController
+
+@end
---
+++ Demo/sViewController.h
@@ -0,0 +1,13 @@
+//
+//  sViewController.h
+//  Demo
+//
+//  Created by apple on 16/11/23.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sViewController : UIViewController
+
+@end
